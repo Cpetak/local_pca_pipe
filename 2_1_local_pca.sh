c8488c49deee7d815d2b8246a1fdd177679d7316
Cpetak <csenge.petak@uvm.edu>
2024-02-06 11:46:58 -0500

    debugged

--- 2_1_local_pca.sh
+++ 2_1_local_pca.sh
@@ -7,14 +7,14 @@ input_dir="/users/c/p/cpetak/EG2023/structural_variation/backup/filtered_bcf_ind
 
 echo $input_dir
 
-for snp in $(seq 500 1000 5000 10000)
+for snp in 500 1000 5000 10000
 do
 	echo "$snp"
 	FILE=$(mktemp)
   	cat header.txt >> $FILE
   	echo "Rscript ~/WGS/local_pca_pipe/run_lostruct.R -i ${input_dir} -t snp -s ${snp} -I ~/WGS/local_pca_pipe/sample_info.tsv -c ${1}" >> $FILE
-    #sbatch $FILE
-  	cat $FILE
+    sbatch $FILE
+  	#cat $FILE
     sleep 0.1
   	rm $FILE
 done
