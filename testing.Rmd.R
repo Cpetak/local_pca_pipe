b02ed5c56a4cc59527212f319b4255bc4b47cc29
Csenge Petak <csenge.petak@uvm.edu>
2024-04-29 10:48:42 -0400

    making report

---
+++ testing.Rmd.R
@@ -0,0 +1,24 @@
+---
+  title: "Figures in R Markdown from PDF"
+author: "Your Name"
+date: "2024-04-29"
+output:
+  html_document:
+  toc: true
+---
+  
+  # Introduction
+  
+  This document demonstrates how to include figures from PDF files in an R Markdown document and surround them with text using headers.
+
+## Figure 1: Scatter Plot
+
+![Figure 1: Scatter Plot](figure1.pdf)
+
+In Figure 1, we present a scatter plot of the data.
+
+## Figure 2: Histogram
+
+![Figure 2: Histogram](figure2.pdf)
+
+Figure 2 displays a histogram of the y variable.
